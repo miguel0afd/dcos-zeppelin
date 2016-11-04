777290b97f6f8f166ccbe09a1b870fc04612bf59
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2016-11-04 22:26:16 +0100

    [CROSSDATA] Minor change

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,6 @@ XD_VERSION=${XD_ZP_VERSION:=1.7.0}
 bin/install-interpreter.sh --name crossdata -t org.apache.zeppelin:zeppelin-crossdata_2.11:$XD_VERSION
 
 cp conf/zeppelin-site.xml.template conf/zeppelin-site.xml
-sed -i 's|org.apache.zeppelin.spark.SparkInterpreter|org.apache.zeppelin.spark.SparkInterpreter,org.apache.zeppelin.crossdata.CrossdataInterpreter|' conf/zeppelin-site.xml
+sed -i 's|org.apache.zeppelin.spark.SparkInterpreter|org.apache.zeppelin.crossdata.CrossdataInterpreter,org.apache.zeppelin.spark.SparkInterpreter|' conf/zeppelin-site.xml
 
 CMD ["bin/zeppelin.sh", "start"]
