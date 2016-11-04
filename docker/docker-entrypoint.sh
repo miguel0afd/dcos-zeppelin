a3c987ee94f731a0e082ac6fad0f5656bf8847ec
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2016-11-04 11:50:53 +0100

    [CROSSDATA] Crossdata interpreter added in docker entry point

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+XD_VERSION=${XD_ZP_VERSION:=1.7.0}
+bin/install-interpreter.sh --name crossdata -t org.apache.zeppelin:zeppelin-crossdata_2.11:$XD_VERSION
+
+cp conf/zeppelin-site.xml.template conf/zeppelin-site.xml
+sed -i 's|org.apache.zeppelin.spark.SparkInterpreter|org.apache.zeppelin.spark.SparkInterpreter,org.apache.zeppelin.crossdata.CrossdataInterpreter|' conf/zeppelin-site.xml
+
+CMD ["bin/zeppelin.sh", "start"]
